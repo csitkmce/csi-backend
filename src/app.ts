1616cf5db37a9a986a5be6cf2eba849da1d8fdc5
nearHaan <farhaan.nizam675@gmail.com>
2025-08-26 01:24:06 +0530

    CORS update

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import indexRoutes from './routes/index.routes.js';
+import express from "express";
+import cors from "cors";
+import indexRoutes from "./routes/index.routes.js";
 import cookieParser from "cookie-parser";
+
+const allowedOrigins = ["http://localhost:5173", "https://csitkmce.vercel.app"];
 
 const app = express();
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use('/api/', indexRoutes);
+app.use("/api/", indexRoutes);
 
 export default app;
