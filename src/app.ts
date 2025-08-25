30175db880b902948c4bdc410aaf66c4e8602b8e
nearHaan <farhaan.nizam675@gmail.com>
2025-08-26 01:30:53 +0530

    CORS update

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,8 @@ app.use(
       }
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
   })
 );
 app.use(express.json());
