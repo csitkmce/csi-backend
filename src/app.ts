93b54e6052d00314146dece69f2635e443ee4134
Aswan-A <aswan0366@gmail.com>
2025-08-27 00:41:21 +0530

    CORS fix 2

--- src/app.ts
+++ src/app.ts
@@ -4,23 +4,28 @@ import indexRoutes from "./routes/index.routes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const isProduction = process.env.NODE_ENV === "production";
 
-if (!isProduction) {
-  const allowedOrigins = ["http://localhost:5173"];
-  const corsOptions = {
-    origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
-      if (!origin) return callback(null, true); // allow non-browser requests
-      if (allowedOrigins.includes(origin)) callback(null, true);
-      else callback(null, false);
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
-    optionsSuccessStatus: 200,
-  };
-  app.use(cors(corsOptions));
-}
+const allowedOrigins = ["http://localhost:5173", "https://csitkmce.vercel.app"];
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin) return callback(null, true); 
+    if (allowedOrigins.includes(origin)) callback(null, true);
+    else callback(null, false);
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "Accept",
+  ],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());
--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -7,6 +7,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const cookieOptions: CookieOptions = {
   httpOnly: true,
   secure: isProduction,
+  sameSite:'lax',
   path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000
 };
