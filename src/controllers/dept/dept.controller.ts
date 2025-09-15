561cb12942dffe2d78a872b2b2913b44838f0f06
Aswan-A <aswan0366@gmail.com>
2025-09-15 18:02:55 +0530

    branch bug fix

--- src/controllers/dept/dept.controller.ts
+++ src/controllers/dept/dept.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 import { pool } from "../../config/db.js";
 
 export const getDepts = async (req: Request, res: Response) => {
-    const query = 'SELECT department_id, department_name FROM departments orderby department_name';
+    const query = 'SELECT department_id, department_name FROM departments order by department_name';
     const { rows } = await pool.query(query);
 
     if (!rows) return res.status(401).json({ error: 'Something went wrong' });
